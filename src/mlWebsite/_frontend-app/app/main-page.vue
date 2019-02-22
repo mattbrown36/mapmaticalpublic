bf66093a0efd5b5e93c71fe0d33d202555e5ce57
Matthew Brown <mattbrown36@gmail.com>
2019-02-21 23:17:39 -0500

    Create main-page.vue

---
+++ src/mlWebsite/_frontend-app/app/main-page.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <top-nav></top-nav>
+
+        <div class="container main-container">
+            <div class="col-sm-12">
+                <div>
+                    <router-view></router-view>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import topNav from "./top-nav.vue";
+    export default {
+        data() {
+            return {};
+        },
+        components: {
+            topNav,
+        },
+        methods : {
+            logout(){
+                this.$http.post('/Account/LogOff').then(function(){
+                    window.location.reload(true);
+                }, function () {
+                    window.location.reload(true);
+                });
+            }
+        },
+    }
+</script>
+
+<style>
+    .main-container {
+        margin-top: 60px;
+        margin-left: 0px;
+        margin-right: 50px;
+        width: 100%;
+        padding-left: 0px;
+    }
+</style>
