808c03d3e16e14487bf23496f31abdbcb0802c05
Matthew Brown <mattbrown36@gmail.com>
2019-02-21 23:25:33 -0500

    Add files via upload

---
+++ src/mlWebsite/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mlWebsite.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
