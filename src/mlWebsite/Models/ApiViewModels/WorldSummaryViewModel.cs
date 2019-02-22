a04e64916f3e531c6c4d9d3f18539d696a7aa7d2
Matthew Brown <mattbrown36@gmail.com>
2019-02-21 23:24:17 -0500

    Create WorldSummaryViewModel.cs

---
+++ src/mlWebsite/Models/ApiViewModels/WorldSummaryViewModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mlWebsite.Models.ApiViewModels
+{
+    public class WorldSummaryViewModel
+    {
+        public Guid id;
+        public string name;
+    }
+}
